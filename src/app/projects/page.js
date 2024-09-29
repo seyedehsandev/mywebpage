9699637957000e09ce60b4568e002f6417f3c243
Ehsan <dev.ehsanhosseini@gmail.com>
2024-09-29 21:03:33 +0330

    edit props

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -6,8 +6,8 @@ function page() {
 
     <div className="flex flex-wrap gap-10 justify-center p-3 md:p-5">
         
-         <ProjectCard title={"Weather App"} img={"/1.jpg"} desc={"This is the best weahter app of all the world"} liveLink={"sourceLinkhttps://weather-woad-sigma.vercel.app/"} sourceLink={"https://github.com/seyedehsandev/weather/tree/master"}/>
-         <ProjectCard title={"Weather App"} img={"/1.jpg"} desc={"This is the best weahter app of all the world"} liveLink={"sourceLinkhttps://weather-woad-sigma.vercel.app/"} sourceLink={"https://github.com/seyedehsandev/weather/tree/master"}/>
+         <ProjectCard title={"Weather App"} img={""} desc={"This is the best weahter app of all the world"} liveLink={"https://weather-woad-sigma.vercel.app/"} sourceLink={"https://github.com/seyedehsandev/weather/tree/master"}/>
+         <ProjectCard title={"Weather App"} img={"/1.jpg"} desc={"This is the best weahter app of all the world"} liveLink={"https://weather-woad-sigma.vercel.app/"} sourceLink={"https://github.com/seyedehsandev/weather/tree/master"}/>
 
 
     </div>
